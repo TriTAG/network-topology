25bf461b5a63b6561df2c34adca5ee3569e2f57d
Mike Boos <mike.boos@gmail.com>
2018-06-05 21:40:30 -0400

    Appease comment-checking linter

--- network_topology/tests/geometry/testbuffer.py
+++ network_topology/tests/geometry/testbuffer.py
@@ -1,3 +1,4 @@
+"""Tests for BufferMaker."""
 import unittest
 from mock import patch
 from network_topology.geometry.buffer import BufferMaker
@@ -5,53 +6,60 @@ from shapely.geometry import LineString, Point, Polygon, MultiPolygon
 
 
 class BufferTestCase(unittest.TestCase):
+    """Test case for BufferMaker."""
+
     def setUp(self):
+        """Create a BufferMaker with a mock logger."""
         self.patcher1 = patch('network_topology.geometry.buffer.logging')
         self.loggerModule = self.patcher1.start()
         self.bufferMaker = BufferMaker()
 
     def tearDown(self):
+        """Disable mock logger."""
         self.patcher1.stop()
 
     def test_buffer_point(self):
+        """Test buffering around a single point."""
         shapes = [Point(0, 0)]
         bufferedShapes = self.bufferMaker._bufferShapes(shapes, thickness=10)
-        diff = abs(bufferedShapes[0].area - 78.5398) / 78.5398  # 5**2 pi
+        diff = abs(bufferedShapes[0].area - 78.5398)/78.5398  # 5**2 pi
         self.assertLess(diff, 0.1)
 
     def test_buffer_line(self):
+        """Test buffering around a line."""
         shapes = [LineString([[0, 0], [100, 0]])]
         bufferedShapes = self.bufferMaker._bufferShapes(shapes, thickness=10)
-        diff = abs(bufferedShapes[0].area - 1078.5398) / 1078.5398
+        diff = abs(bufferedShapes[0].area - 1078.5398)/1078.5398
         self.assertLess(diff, 0.01)
 
     def test_remove_holes_polygon(self):
+        """Test removing small internal holes."""
         bigShape = Polygon([[0, 0], [100, 0], [100, 100], [0, 100]],
                            [[[10, 10], [20, 10], [20, 20], [10, 20]],
                             [[90, 90], [90, 85], [85, 85], [85, 90]]])
-        filledShape = self.bufferMaker._removeHoles(
-            bigShape, minInnerPerimeter=21)
+        filledShape = self.bufferMaker._removeHoles(bigShape,
+                                                    minInnerPerimeter=21)
         self.assertEqual(len(filledShape.interiors), 1)
 
     def test_remove_holes_multipolygon(self):
+        """Test removing small internal holes from multiple polygons."""
         bigShape = MultiPolygon([([[0, 0], [100, 0], [100, 100], [0, 100]],
                                   [[[10, 10], [20, 10], [20, 20], [10, 20]]]),
                                  ([[200, 0], [300, 0], [300, 100], [200, 100]],
-                                  [[[210, 10], [215, 10], [215, 15], [210,
-                                                                      15]]])])
-        filledShape = self.bufferMaker._removeHoles(
-            bigShape, minInnerPerimeter=21)
+                                  [[[210, 10], [215, 10], [215, 15],
+                                    [210, 15]]])])
+        filledShape = self.bufferMaker._removeHoles(bigShape,
+                                                    minInnerPerimeter=21)
         self.assertEqual(len(filledShape.geoms), 2)
         self.assertEqual(len(filledShape.geoms[0].interiors), 1)
         self.assertEqual(len(filledShape.geoms[1].interiors), 0)
 
     def test_buffer_end_to_end(self):
-        ls = [
-            LineString([[0, 0], [100, 0]]),
-            LineString([[10, 10], [10, -30]]),
-            LineString([[20, 10], [20, -30]]),
-            LineString([[0, -10], [100, -10]]),
-            LineString([[40, 10], [40, -30]])
-        ]
+        """Test BufferMaker in an end-to-end scenario."""
+        ls = [LineString([[0, 0], [100, 0]]),
+              LineString([[10, 10], [10, -30]]),
+              LineString([[20, 10], [20, -30]]),
+              LineString([[0, -10], [100, -10]]),
+              LineString([[40, 10], [40, -30]])]
         bufferedShape = self.bufferMaker.makeBufferedShape(ls, 5, 25)
         self.assertEqual(len(bufferedShape.interiors), 1)
--- network_topology/tests/geometry/testedgeiterator.py
+++ network_topology/tests/geometry/testedgeiterator.py
@@ -1,8 +1,12 @@
+"""Tests for EdgeIterator class."""
 import unittest
 from network_topology.geometry.edgeiterator import EdgeIterator
 
 
 class EdgeIterTestCase(unittest.TestCase):
+    """Test of edge iterator."""
+
     def test_edge_iter(self):
+        """Test that source,target are always expressed in ascending order."""
         ei = EdgeIterator([0, 1, 2])
         self.assertSequenceEqual(list(ei), [(0, 1), (1, 2), (0, 2)])
