384423a7c13a1975bf18d059c255b26e8928e50a
Mike Boos <mike.boos@gmail.com>
2018-06-05 21:42:45 -0400

    Replace Mesh class node sequence internals with graph

--- network_topology/geometry/mesh.py
+++ network_topology/geometry/mesh.py
@@ -1,5 +1,6 @@
 """Class to store and manipulate a mesh."""
 
+import networkx as nx
 from collections import defaultdict
 from shapely.geometry import Polygon
 from edgeiterator import EdgeIterator
@@ -8,7 +9,7 @@ from edgeiterator import EdgeIterator
 class Mesh(object):
     """Class to store and manipulate a mesh."""
 
-    def __init__(self, vertices, polygons, edge_nodes):
+    def __init__(self, vertices, edge_nodes, graph):
         """Initialize mesh object.
 
         Parameters
@@ -18,18 +19,20 @@ class Mesh(object):
         polygons : sequence
             A sequence of vertex index sequences
         edge_nodes:
+            A sequence of:
+                1 if coordinate is on an outer edge
+                0 otherwise
+        graph:
+            A graph represending topological connections between points
         """
         self._vertices = vertices
-        self._polygons = polygons
+        self._graph = graph
         self._edge_nodes = edge_nodes
-
-    def isInternal(self, nodeId):
-        """Return true if the given vertex index is internal to the shape."""
-        return not self._edge_nodes[nodeId]
 
     def polygons(self):
         """Return an iterator of polygon vertex lists."""
-        return iter(self._polygons)
+        for p in self._graph:
+            yield self._graph.node[p]['vertices']
 
     def getVertex(self, nodeId):
         """Return coordinates of the given vertex."""
@@ -37,9 +40,30 @@ class Mesh(object):
 
     def getShape(self, polyId):
         """Generate shapely object of the given polygon."""
-        return Polygon([self._vertices[v] for v in self._polygons[polyId]])
+        return Polygon([self._vertices[v]
+                        for v in self._graph.node[polyId]['vertices']])
 
-    def mergePolygons(self, polyIds):
+    def collapseShapes(self):
+        """Merge polygons that are fully internal or have internal nodes."""
+        polygons = self._findPolygonsToMerge()
+        subgraph = self._graph.subgraph(polygons)
+        for cluster in nx.connected_components(subgraph):
+            self._mergeCluster(cluster)
+
+    def _findPolygonsToMerge(self):
+        """Return indexes of all polygons that share all their edges."""
+        internal = []
+        for poly, data in self._graph.nodes(data=True):
+            if (len(self._graph[poly]) == len(data['vertices']) or
+                    any(map(self._isInternal, data['vertices']))):
+                internal.append(poly)
+        return internal
+
+    def _isInternal(self, nodeId):
+        """Return true if the given vertex index is internal to the shape."""
+        return not self._edge_nodes[nodeId]
+
+    def _mergeCluster(self, polyIds):
         """Combine adjacent polygons into a single one.
 
         Parameters
@@ -47,11 +71,19 @@ class Mesh(object):
         polyIds : sequence
             A sequence of index values of adjacent polygons
         """
+        polyIds = list(polyIds)
+        base = polyIds[0]
         edges = defaultdict(list)
+
         for poly in polyIds:
-            nodes = self._polygons[poly]
+            for neighbour, data in self._graph[poly].items():
+                if neighbour not in polyIds:
+                    self._graph.add_edge(base, neighbour, **data)
+            nodes = self._graph.node[poly]['vertices']
             for edge in EdgeIterator(nodes):
                 edges[edge].append(poly)
+            if poly != base:
+                self._graph.remove_node(poly)
         neighbours = defaultdict(list)
         for (n1, n2), polys in edges.items():
             if len(polys) == 1:
@@ -65,6 +97,4 @@ class Mesh(object):
                 if neighbour not in nodes:
                     nodes.append(neighbour)
                     current = neighbour
-        self._polygons[polyIds[0]] = nodes
-        for poly in polyIds[1:]:
-            self._polygons[poly] = []
+        self._graph.nodes[base]['vertices'] = nodes
--- network_topology/tests/geometry/testmesh.py
+++ network_topology/tests/geometry/testmesh.py
@@ -1,14 +1,21 @@
 import unittest
 # from mock import Mock, patch
+import networkx as nx
 from network_topology.geometry.mesh import Mesh
-
 
 class MeshTestCase(unittest.TestCase):
     def setUp(self):
+        graph = nx.Graph()
+        graph.add_node(0, vertices=[0, 1, 4])
+        graph.add_node(1, vertices=[1, 2, 4])
+        graph.add_node(2, vertices=[2, 3, 4])
+        graph.add_node(3, vertices=[3, 4, 0])
+        graph.add_edge(0, 1, common=(1, 4))
+        graph.add_edge(0, 3, common=(0, 4))
+        graph.add_edge(1, 2, common=(2, 4))
+        graph.add_edge(2, 3, common=(1, 4))
         self.mesh = Mesh([[0, 0], [0, 2], [2, 2], [2, 0], [1, 1]],
-                         [[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 4, 0]],
-                         [1, 1, 1, 1, 0])
-
+                         [1, 1, 1, 1, 0], graph)
     #     #self.patcher1 = patch('network_topology.geometry.buffer.logging')
     #     #self.loggerModule = self.patcher1.start()
     #     #self.bufferMaker = BufferMaker()
@@ -17,21 +24,33 @@ class MeshTestCase(unittest.TestCase):
     #     self.patcher1.stop()
 
     def test_internal_node(self):
-        self.assertTrue(self.mesh.isInternal(4))
+        #print self.mesh._edge_nodes
+        self.assertTrue(self.mesh._isInternal(4))
 
     def test_external_node(self):
-        self.assertFalse(self.mesh.isInternal(0))
+        self.assertFalse(self.mesh._isInternal(0))
 
     def test_merge(self):
-        self.mesh.mergePolygons([0, 1])
-        polygons = [[0, 1, 2, 4], [], [2, 3, 4], [3, 4, 0]]
+        self.mesh._mergeCluster([0, 1])
+        polygons = [[0, 1, 2, 4], [2, 3, 4], [3, 4, 0]]
         for canon, candidate in zip(polygons, self.mesh.polygons()):
             self.assertSetEqual(set(canon), set(candidate))
 
     def test_full_merge(self):
-        self.mesh.mergePolygons([0, 1, 2, 3])
-        polygons = [[0, 1, 2, 3], [], [], []]
+        self.mesh._mergeCluster([0, 1, 2, 3])
+        polygons = [[0, 1, 2, 3]]
         for canon, candidate in zip(polygons, self.mesh.polygons()):
+            self.assertSetEqual(set(canon), set(candidate))
+
+    def test_collapse(self):
+        self.mesh.collapseShapes()
+        polygons = [[0, 1, 2, 3]]
+        for canon, candidate in zip(polygons, self.mesh.polygons()):
+            self.assertSetEqual(set(canon), set(candidate))
+
+    def test_polygons(self):
+        polygons = [[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 4, 0]]
+        for canon, candidate in zip(polygons, list(self.mesh.polygons())):
             self.assertSetEqual(set(canon), set(candidate))
 
     def test_get_vertex(self):
